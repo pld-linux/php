49d6a4cde17eeb3f77f59a0201fa3ef44cf3258a
Elan Ruusamäe <glen@pld-linux.org>
2009-12-02 21:21:49 +0000

    - inter deps checker. for php 5.2
    Changed files:
        dep-tests.sh -> 1.1

---
+++ dep-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# inter-dependencies checker.
+
+# spl must be loaded before simplexml, therefore spl has itself before its deps
+dep_spl="pcre spl simplexml"
+dep_filter='pcre'
+dep_eaccelerator='session'
+dep_mysqli="$dep_spl"
+dep_pdo="$dep_spl"
+dep_pdo_sqlite="$dep_pdo pdo"
+dep_pdo_pgsql="$dep_pdo pdo"
+dep_pdo_odbc="$dep_pdo pdo"
+dep_pdo_firebird="$dep_pdo pdo"
+dep_pdo_dblib="$dep_pdo pdo"
+dep_pdo_mysql="$dep_pdo pdo"
+dep_simplexml="$dep_pdo pdo"
+dep_sqlite="$dep_pdo pdo"
+dep_wddx='xml'
+dep_xmlreader='dom'
+dep_xmlrpc='xml'
+dep_xsl='dom'
+
+php=${PHP:-$(php-config --php-binary)}
+ext_dir=${EXTENSION_DIR:-$(php-config --extension-dir)}
+
+# poldek --sn ac-ready -u php-*
+for ext in $ext_dir/*.so; do
+	[ -f $ext ] || continue
+	ext=${ext##*/}; ext=${ext%.so}
+	deps=$(eval echo \$dep_$ext)
+	# add ext itself, if already not in list (spl case)
+	[[ $deps = *$ext* ]] || deps="$deps $ext"
+	args=$(for e in $deps; do echo -d extension=$e.so; done)
+	echo -n "$ext (deps: ${deps# })..."
+	$php -n -d extension_dir=$ext_dir $args -r "exit(extension_loaded('${ext}') ? 0 : 1);" && echo OK || echo FAIL
+done

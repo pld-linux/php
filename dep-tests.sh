7dac1fa2e5ecfafbcd401f802587e56a13f0ad06
cvs2git <feedback@pld-linux.org>
2010-06-13 21:33:31 +0000

    This commit was manufactured by cvs2git to create tag 'auto-th-php- 5_2_11-12'.
    Sprout from PHP_5_2 2010-06-13 21:33:31 UTC cvs2git <feedback@pld-linux.org> 'This commit was manufactured by cvs2git to create branch 'PHP_5_2'.'
    Cherrypick from PHP_5_2 2009-12-02 22:37:03 UTC Elan Ruusamäe <glen@pld-linux.org> '- core no longer needs to load simplexml.so for self-tests':
        php-fpm.patch -> 1.14
        php-no-metaccld.patch -> 1.4
        php.spec -> 1.805.2.29
        suhosin.patch -> 1.2
    Cherrypick from master 2009-12-02 21:21:49 UTC Elan Ruusamäe <glen@pld-linux.org> '- inter deps checker. for php 5.2':
        dep-tests.sh -> 1.1
        php_browscap.ini -> 1.5
    Delete:
        bug-52078-fileinode.patch
        fix-test-run.patch
        php-gmp.patch
        php-libpng.patch
        skip-tests.sh

--- dep-tests.sh
+++ dep-tests.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # inter-dependencies checker.
 
-dep_spl="pcre simplexml"
+# spl must be loaded before simplexml, therefore spl has itself before its deps
+dep_spl="pcre spl simplexml"
 dep_filter='pcre'
 dep_eaccelerator='session'
-dep_mysqli="$dep_spl spl"
-dep_pdo="$dep_spl spl"
+dep_mysqli="$dep_spl"
+dep_pdo="$dep_spl"
 dep_pdo_sqlite="$dep_pdo pdo"
 dep_pdo_pgsql="$dep_pdo pdo"
-dep_pdo_oci="$dep_pdo pdo"
 dep_pdo_odbc="$dep_pdo pdo"
 dep_pdo_firebird="$dep_pdo pdo"
 dep_pdo_dblib="$dep_pdo pdo"
 dep_pdo_mysql="$dep_pdo pdo"
-dep_simplexml="$dep_spl spl"
+dep_simplexml="$dep_pdo pdo"
 dep_sqlite="$dep_pdo pdo"
 dep_wddx='xml'
 dep_xmlreader='dom'
@@ -22,29 +22,15 @@ dep_xsl='dom'
 
 php=${PHP:-$(php-config --php-binary)}
 ext_dir=${EXTENSION_DIR:-$(php-config --extension-dir)}
-conf_dir=${CONFIG_DIR:-$(php-config --sysconfdir)/conf.d $(php-config --sysconfdir)/cli.d}
-tmpini=$(mktemp)
 
 # poldek --sn ac-ready -u php-*
 for ext in $ext_dir/*.so; do
 	[ -f $ext ] || continue
 	ext=${ext##*/}; ext=${ext%.so}
-
 	deps=$(eval echo \$dep_$ext)
 	# add ext itself, if already not in list (spl case)
 	[[ $deps = *$ext* ]] || deps="$deps $ext"
-
+	args=$(for e in $deps; do echo -d extension=$e.so; done)
 	echo -n "$ext (deps: ${deps# })..."
-
-	grep -rlE '^extension=('$(echo "${deps# }" | tr ' ' '|')').so$' $conf_dir | LC_CTYPE=C LC_ALL= sort | xargs cat > $tmpini
-	$php -n -d extension_dir=$ext_dir -c $tmpini -r "exit(extension_loaded('${ext}') ? 0 : 1);"
-	rc=$?
-	if [ $rc = 0 ]; then
-		echo OK
-	else
-		echo FAIL
-		echo "Failed config was:"
-		cat $tmpini
-	fi
+	$php -n -d extension_dir=$ext_dir $args -r "exit(extension_loaded('${ext}') ? 0 : 1);" && echo OK || echo FAIL
 done
-rm -f $t

debbc45e580b703a4076164ec602a440943f3e73
cvs2git <feedback@pld-linux.org>
2010-06-13 21:33:31 +0000

    This commit was manufactured by cvs2git to create branch 'PHP_5_2'.
    Cherrypick from master 2009-10-18 17:04:40 UTC Elan Ruusamäe <glen@pld-linux.org> '- pdo R spl; rel 7':
        extension-shared-optional-dep.patch -> 1.1
        php-branch.sh -> 1.5
        php-silent-session-cleanup.patch -> 1.1
        spl-shared.patch -> 1.3
    Cherrypick from master 2010-06-07 19:36:29 UTC Elan Ruusamäe <glen@pld-linux.org> '- load pcre from system extension dir':
        fix-test-run.patch -> 1.6
        skip-tests.sh -> 1.4
    Cherrypick from master 2009-08-18 10:22:20 UTC Arkadiusz Miśkiewicz <arekm@maven.pl> '- rel 10; fix build with autoconf 2.64':
        php-m4-divert.patch -> 1.1
    Cherrypick from master 2010-02-10 11:59:33 UTC Jan Rękorajski <baggins@pld-linux.org> '- added pdo_oci':
        dep-tests.sh -> 1.4
    Cherrypick from master 2010-06-13 21:33:31 UTC Elan Ruusamäe <glen@pld-linux.org> '- fix ext/standard/tests/file/fileinode_variation3.phpt test':
        bug-52078-fileinode.patch -> 1.1

---
+++ dep-tests.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# inter-dependencies checker.
+
+dep_spl="pcre simplexml"
+dep_filter='pcre'
+dep_eaccelerator='session'
+dep_mysqli="$dep_spl spl"
+dep_pdo="$dep_spl spl"
+dep_pdo_sqlite="$dep_pdo pdo"
+dep_pdo_pgsql="$dep_pdo pdo"
+dep_pdo_oci="$dep_pdo pdo"
+dep_pdo_odbc="$dep_pdo pdo"
+dep_pdo_firebird="$dep_pdo pdo"
+dep_pdo_dblib="$dep_pdo pdo"
+dep_pdo_mysql="$dep_pdo pdo"
+dep_simplexml="$dep_spl spl"
+dep_sqlite="$dep_pdo pdo"
+dep_wddx='xml'
+dep_xmlreader='dom'
+dep_xmlrpc='xml'
+dep_xsl='dom'
+
+php=${PHP:-$(php-config --php-binary)}
+ext_dir=${EXTENSION_DIR:-$(php-config --extension-dir)}
+conf_dir=${CONFIG_DIR:-$(php-config --sysconfdir)/conf.d $(php-config --sysconfdir)/cli.d}
+tmpini=$(mktemp)
+
+# poldek --sn ac-ready -u php-*
+for ext in $ext_dir/*.so; do
+	[ -f $ext ] || continue
+	ext=${ext##*/}; ext=${ext%.so}
+
+	deps=$(eval echo \$dep_$ext)
+	# add ext itself, if already not in list (spl case)
+	[[ $deps = *$ext* ]] || deps="$deps $ext"
+
+	echo -n "$ext (deps: ${deps# })..."
+
+	grep -rlE '^extension=('$(echo "${deps# }" | tr ' ' '|')').so$' $conf_dir | LC_CTYPE=C LC_ALL= sort | xargs cat > $tmpini
+	$php -n -d extension_dir=$ext_dir -c $tmpini -r "exit(extension_loaded('${ext}') ? 0 : 1);"
+	rc=$?
+	if [ $rc = 0 ]; then
+		echo OK
+	else
+		echo FAIL
+		echo "Failed config was:"
+		cat $tmpini
+	fi
+done
+rm -f $t

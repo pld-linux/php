463492bcb913e9486cc410419776eba995c49eca
Elan Ruusamäe <glen@pld-linux.org>
2010-07-25 13:19:59 +0000

    - update to 5.3.3 (th-x86_64)
    Changed files:
        skip-tests.sh -> 1.6

--- skip-tests.sh
+++ skip-tests.sh
@@ -1,17 +1,40 @@
+#!/bin/sh
+# Class constant whose initial value refereces a non-existent class
+mv tests/classes/constants_error_004.phpt{,.skip}
 # easter_date()
 mv ext/calendar/tests/easter_date.phpt{,.skip}
 # unixtojd()
 mv ext/calendar/tests/unixtojd.phpt{,.skip}
+# curl_error() function - basic test for curl_error using a fake url
+mv ext/curl/tests/curl_error_basic.phpt{,.skip}
+# curl_setopt() basic parameter test
+mv ext/curl/tests/curl_setopt_error.phpt{,.skip}
+# timezone_abbreviations_list() tests
+mv ext/date/tests/010.phpt{,.skip}
 # Test DateTime::modify() function : usage variation - Passing unexpected values to first argument $modify.
 mv ext/date/tests/DateTime_modify_variation1.phpt{,.skip}
-# Bug #50392 date_create_from_format enforces 6 digits for 'u' format character
-mv ext/date/tests/bug50392.phpt{,.skip}
+# Bug #48187 (DateTime::diff() corrupting microtime() result)
+mv ext/date/tests/bug48187.phpt{,.skip}
+# Bug #50475 (DateTime::setISODate followed by DateTime::setTime)
+mv ext/date/tests/bug50475.phpt{,.skip}
+# Bug #51819 (Case discrepancy in timezone names cause Uncaught exception and fatal error)
+mv ext/date/tests/bug51819.phpt{,.skip}
+# Bug #51994 (date_parse_from_format is parsing invalid date using 'yz' format)
+mv ext/date/tests/bug51994.phpt{,.skip}
+# Bug #52290 (setDate, setISODate, setTime works wrong when DateTime created from timestamp)
+mv ext/date/tests/bug52290.phpt{,.skip}
 # Test date_modify() function : usage variation - Passing unexpected values to second argument $format.
 mv ext/date/tests/date_modify_variation2.phpt{,.skip}
+# Test timezone_abbreviations_list() function : basic functionality
+mv ext/date/tests/timezone_abbreviations_list_basic1.phpt{,.skip}
+# Test 5: HTML Test
+mv ext/dom/tests/dom005.phpt{,.skip}
 # Bug #48555 (ImageFTBBox() differs from previous versions for texts with new lines)
 mv ext/gd/tests/bug48555.phpt{,.skip}
 # Feature Request #50283 (allow base in gmp_strval to use full range: 2 to 62, and -2 to -36)
 mv ext/gmp/tests/bug50283.phpt{,.skip}
+# gmp_nextprime()
+mv ext/gmp/tests/gmp_nextprime.phpt{,.skip}
 # InterBase: connect, close and pconnect
 mv ext/interbase/tests/002.phpt{,.skip}
 # InterBase: misc sql types (may take a while)
@@ -60,6 +83,10 @@ mv ext/interbase/tests/ibase_trans_002.phpt{,.skip}
 mv ext/intl/tests/collator_get_locale.phpt{,.skip}
 # collator_get_sort_key()
 mv ext/intl/tests/collator_get_sort_key.phpt{,.skip}
+# datefmt_format_code() and datefmt_parse_code()
+mv ext/intl/tests/dateformat_format_parse.phpt{,.skip}
+# datefmt_get_pattern_code and datefmt_set_pattern_code()
+mv ext/intl/tests/dateformat_get_set_pattern.phpt{,.skip}
 # datefmt_localtime_code()
 mv ext/intl/tests/dateformat_localtime.phpt{,.skip}
 # datefmt_parse_code()
@@ -74,16 +101,12 @@ mv ext/intl/tests/dateformat_set_timezone_id.phpt{,.skip}
 mv ext/intl/tests/formatter_format.phpt{,.skip}
 # numfmt_format_currency()
 mv ext/intl/tests/formatter_format_currency.phpt{,.skip}
-# numfmt_get/set_symbol()
-mv ext/intl/tests/formatter_get_set_symbol.phpt{,.skip}
-# numfmt_parse_currency()
-mv ext/intl/tests/formatter_parse_currency.phpt{,.skip}
-# locale_get_display_language()
-mv ext/intl/tests/locale_get_display_language.phpt{,.skip}
+# numfmt_get/set_attribute()
+mv ext/intl/tests/formatter_get_set_attribute.phpt{,.skip}
+# grapheme()
+mv ext/intl/tests/grapheme.phpt{,.skip}
 # locale_get_display_name()
 mv ext/intl/tests/locale_get_display_name.phpt{,.skip}
-# locale_get_display_region()
-mv ext/intl/tests/locale_get_display_region.phpt{,.skip}
 # locale_get_display_script()
 mv ext/intl/tests/locale_get_display_script.phpt{,.skip}
 # locale_get_display_variant()
@@ -104,10 +127,12 @@ mv ext/mysqli/tests/mysqli_connect.phpt{,.skip}
 mv ext/mysqli/tests/mysqli_connect_oo.phpt{,.skip}
 # new mysqli()
 mv ext/mysqli/tests/mysqli_connect_oo_defaults.phpt{,.skip}
-# pcntl_exec()
-mv ext/pcntl/tests/pcntl_exec.phpt{,.skip}
-# pcntl_exec() 2
-mv ext/pcntl/tests/pcntl_exec_2.phpt{,.skip}
+# Bug #28382 (openssl_x509_parse extensions support)
+mv ext/openssl/tests/bug28382.phpt{,.skip}
+# Bug #47828 (segfaults when a UTF-8 conversion fails openssl_x509_parse())
+mv ext/openssl/tests/bug47828.phpt{,.skip}
+# openssl_x509_parse() basic test
+mv ext/openssl/tests/openssl_x509_parse_basic.phpt{,.skip}
 # PDO_Firebird: connect/disconnect
 mv ext/pdo_firebird/tests/connect.phpt{,.skip}
 # PDO_Firebird: DDL/transactions
@@ -116,14 +141,44 @@ mv ext/pdo_firebird/tests/ddl.phpt{,.skip}
 mv ext/pdo_firebird/tests/execute.phpt{,.skip}
 # MySQL PDO->__construct(), options
 mv ext/pdo_mysql/tests/pdo_mysql___construct_options.phpt{,.skip}
+# MySQL PDO->__construct(), libmysql only options
+mv ext/pdo_mysql/tests/pdo_mysql___construct_options_libmysql.phpt{,.skip}
 # MySQL PDO class interface
 mv ext/pdo_mysql/tests/pdo_mysql_interface.phpt{,.skip}
 # PDO ODBC "long" columns
 mv ext/pdo_odbc/tests/long_columns.phpt{,.skip}
 # PDO SQLite Feature Request #42589 (getColumnMeta() should also return table name)
 mv ext/pdo_sqlite/tests/bug_42589.phpt{,.skip}
+# Test for bug 52013 about Phar::decompressFiles().
+mv ext/phar/tests/bug52013.phpt{,.skip}
+# Phar and RecursiveDirectoryIterator
+mv ext/phar/tests/phar_oo_005.phpt{,.skip}
 # Test posix_getgrgid() function : basic functionality
 mv ext/posix/tests/posix_getgrgid_basic.phpt{,.skip}
+# ReflectionClass::getConstructor()
+mv ext/reflection/tests/ReflectionClass_getConstructor_basic.phpt{,.skip}
+# ReflectionMethod::isConstructor()
+mv ext/reflection/tests/ReflectionMethod_constructor_basic.phpt{,.skip}
+# ReflectionObject::getConstructor() - basic function test
+mv ext/reflection/tests/ReflectionObject_getConstructor_basic.phpt{,.skip}
+# a script should not be able to modify session.use_trans_sid
+mv ext/session/tests/014.phpt{,.skip}
+# use_trans_sid should not affect SID
+mv ext/session/tests/015.phpt{,.skip}
+# rewriter correctly handles attribute names which contain dashes
+mv ext/session/tests/018.phpt{,.skip}
+# rewriter uses arg_seperator.output for modifying URLs
+mv ext/session/tests/020.phpt{,.skip}
+# rewriter handles form and fieldset tags correctly
+mv ext/session/tests/021.phpt{,.skip}
+# Bug #31454 (Incorrect adding PHPSESSID to links, which contains \r\n)
+mv ext/session/tests/bug36459.phpt{,.skip}
+# Bug #41600 (url rewriter tags doesn't work with namespaced tags)
+mv ext/session/tests/bug41600.phpt{,.skip}
+# Test session_encode() function : error functionality
+mv ext/session/tests/session_encode_error2.phpt{,.skip}
+# SimpleXML: XPath
+mv ext/simplexml/tests/008.phpt{,.skip}
 # SOAP Server 9: setclass and setpersistence(SOAP_PERSISTENCE_SESSION)
 mv ext/soap/tests/server009.phpt{,.skip}
 # ext/sockets - socket_strerror - basic test
@@ -140,14 +195,18 @@ mv ext/sqlite3/tests/sqlite3_33_load_extension_param.phpt{,.skip}
 mv ext/sqlite3/tests/sqlite3_34_load_extension_ext_dir.phpt{,.skip}
 # SQLite3::loadExtension test with wrong parameter type
 mv ext/sqlite3/tests/sqlite3_loadextension_with_wrong_param.phpt{,.skip}
-# Test filetype() function: Check character type
-mv ext/standard/tests/file/filetype_variation2.phpt{,.skip}
-# realpath_cache_size() and realpath_cache_get()
-mv ext/standard/tests/file/realpath_cache.phpt{,.skip}
+# Test fscanf() function: usage variations - unsigned int formats with integer values
+mv ext/standard/tests/file/fscanf_variation39.phpt{,.skip}
+# Test fscanf() function: usage variations - tracking file pointer while reading
+mv ext/standard/tests/file/fscanf_variation55.phpt{,.skip}
+# Bug #44394 (Last two bytes missing from output) with session.use_trans_id
+mv ext/standard/tests/general_functions/bug44394_2.phpt{,.skip}
+# Test function getservbyname()
+mv ext/standard/tests/general_functions/getservbyname_basic.phpt{,.skip}
 # proc_open
 mv ext/standard/tests/general_functions/proc_open02.phpt{,.skip}
-# Test Blowfish crypt() with invalid rounds
-mv ext/standard/tests/strings/crypt_blowfish_invalid_rounds.phpt{,.skip}
+# time_sleep_until() function - basic test for time_sleep_until()
+mv ext/standard/tests/misc/time_sleep_until_basic.phpt{,.skip}
 # htmlentities() test 2 (setlocale / fr_FR.ISO-8859-15)
 mv ext/standard/tests/strings/htmlentities02.phpt{,.skip}
 # htmlentities() test 4 (setlocale / ja_JP.EUC-JP)
@@ -164,24 +223,20 @@ mv ext/standard/tests/strings/htmlentities15.phpt{,.skip}
 mv ext/standard/tests/strings/htmlentities17.phpt{,.skip}
 # Test setlocale() function : usage variations - Setting all available locales in the platform
 mv ext/standard/tests/strings/setlocale_variation2.phpt{,.skip}
+# Test sscanf() function : basic functionality - unsigned format
+mv ext/standard/tests/strings/sscanf_basic6.phpt{,.skip}
+# msg_send() data types when not serializing
+mv ext/sysvmsg/tests/006.phpt{,.skip}
 # wddx session serializer handler (serialize)
 mv ext/wddx/tests/004.phpt{,.skip}
 # wddx session serializer handler (deserialize)
 mv ext/wddx/tests/005.phpt{,.skip}
-# Bug #35447 (xml_parse_into_struct() chokes on the UTF-8 BOM)
-mv ext/xml/tests/bug35447.phpt{,.skip}
-# XML Parser test: concat character data and set empty handlers
-mv ext/xml/tests/xml011.phpt{,.skip}
 # xmlrpc_encode_request() and various arguments
 mv ext/xmlrpc/tests/002.phpt{,.skip}
 # Bug #40576 (double values are truncated to 6 decimal digits when encoding)
-mv ext/xmlrpc/tests/bug40576.phpt{,.skip}
-# Bug #42189 (xmlrpc_get_type() crashes PHP on invalid dates)
-mv ext/xmlrpc/tests/bug42189.phpt{,.skip}
-# Bug #44996 (xmlrpc_decode() ignores time zone on iso8601.datetime)
-mv ext/xmlrpc/tests/bug44996.phpt{,.skip}
-# Bug #45226 (xmlrpc_set_type() segfaults with valid ISO8601 date string)
-mv ext/xmlrpc/tests/bug45226.phpt{,.skip}
+mv ext/xmlrpc/tests/bug40576_64bit.phpt{,.skip}
+# Bug #45555 (Segfault with invalid non-string as register_introspection_callback)
+mv ext/xmlrpc/tests/bug45555.phpt{,.skip}
 # Bug #45556 (Return value from callback isn't freed)
 mv ext/xmlrpc/tests/bug45556.phpt{,.skip}
 # Test 10: EXSLT Support
@@ -194,47 +249,3 @@ mv ext/xsl/tests/xsltprocessor_registerPHPFunctions-funcundef.phpt{,.skip}
 mv sapi/cli/tests/006.phpt{,.skip}
 # CLI -a and readline
 mv sapi/cli/tests/016.phpt{,.skip}
-# CLI php -m
-mv sapi/cli/tests/018.phpt{,.skip}
-# Test open_basedir configuration
-mv tests/security/open_basedir_fileinode.phpt{,.skip}
-# Test fileinode() function: Variations
-mv ext/standard/tests/file/fileinode_variation.phpt{,.skip}
-# Test lstat() and stat() functions: usage variations - dir/file name stored in object
-mv ext/standard/tests/file/lstat_stat_variation18.phpt{,.skip}
-# Test lstat() and stat() functions: usage variations - dir/file names in array
-mv ext/standard/tests/file/lstat_stat_variation19.phpt{,.skip}
-# Test fileinode() function: usage variations - diff. path notations
-mv ext/standard/tests/file/fileinode_variation3.phpt{,.skip}
-# curl_error() function - basic test for curl_error using a fake url
-mv ext/curl/tests/curl_error_basic.phpt{,.skip}
-# gmp_nextprime()
-mv ext/gmp/tests/gmp_nextprime.phpt{,.skip}
-# datefmt_format_code() and datefmt_parse_code()
-mv ext/intl/tests/dateformat_format_parse.phpt{,.skip}
-# datefmt_get_pattern_code and datefmt_set_pattern_code()
-mv ext/intl/tests/dateformat_get_set_pattern.phpt{,.skip}
-# numfmt_get/set_attribute()
-mv ext/intl/tests/formatter_get_set_attribute.phpt{,.skip}
-# grapheme()
-mv ext/intl/tests/grapheme.phpt{,.skip}
-# Bug #28382 (openssl_x509_parse extensions support)
-mv ext/openssl/tests/bug28382.phpt{,.skip}
-# Bug #47828 (segfaults when a UTF-8 conversion fails openssl_x509_parse())
-mv ext/openssl/tests/bug47828.phpt{,.skip}
-# openssl_x509_parse() basic test
-mv ext/openssl/tests/openssl_x509_parse_basic.phpt{,.skip}
-# MySQL PDO->__construct(), libmysql only options
-mv ext/pdo_mysql/tests/pdo_mysql___construct_options_libmysql.phpt{,.skip}
-# Test function getservbyport() by calling it more than or less than its expected arguments
-mv ext/standard/tests/general_functions/getservbyname_basic.phpt{,.skip}
-# Test var_dump() function
-mv ext/standard/tests/general_functions/var_dump_64bit.phpt{,.skip}
-# time_sleep_until() function - basic test for time_sleep_until()
-mv ext/standard/tests/misc/time_sleep_until_basic.phpt{,.skip}
-# getmxrr() test
-mv ext/standard/tests/network/getmxrr.phpt{,.skip}
-# Bug #40576 (double values are truncated to 6 decimal digits when encoding)
-mv ext/xmlrpc/tests/bug40576_64bit.phpt{,.skip}
-# Bug #45555 (Segfault with invalid non-string as register_introspection_callback)
-mv ext/xmlrpc/tests/bug45555.phpt{,.skip}
